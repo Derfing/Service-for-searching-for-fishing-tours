fc7b6a3d6eff8b417db404b5536f6e8d5569e620
Derfing <derendaevkosta45@gmail.com>
2023-04-03 01:17:10 +0300

    edit layout to main page and add multiple select

--- backend/index.php
+++ backend/index.php
@@ -6,20 +6,22 @@ use classes\Tariff;
 
 include('ConnectToDatabase.php');
 
-$name = $connection->real_escape_string($_POST['name']);
-$type = $connection->real_escape_string($_POST['type']);
+$name = $_POST['name'];
+$types = $_POST['types'];
 
-if ($name && !$type)
-    $query = "SELECT * FROM waterbody WHERE waterbody_name='$name'";
-elseif ($name && $type)
-    $query = "SELECT * FROM waterbody WHERE waterbody_name='$name' OR waterbody_type='$type'";
+echo var_dump($types);
 
-$result = $connection->query($query);
+// if ($name && !$type)
+//     $query = "SELECT * FROM waterbody WHERE waterbody_name='$name'";
+// elseif ($name && $type)
+//     $query = "SELECT * FROM waterbody WHERE waterbody_name='$name' OR waterbody_type='$type'";
 
-if (!$connection->errno) {
-    foreach ($result as $row) {
-        echo $row['waterbody_id'] . ' ' . $row['area'] . ' ' . $row['waterbody_name'] . '<br>';
-    }
-} else {
-    echo $connection->error;
-}
+// $result = $connection->query($query);
+
+// if (!$connection->errno) {
+//     foreach ($result as $row) {
+//         echo $row['waterbody_id'] . ' ' . $row['area'] . ' ' . $row['waterbody_name'] . '<br>';
+//     }
+// } else {
+//     echo $connection->error;
+// }
