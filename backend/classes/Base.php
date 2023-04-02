f2139fd86a60ad1c842bec5037f87c1d6b7846d9
Derfing <derendaevkosta45@gmail.com>
2023-04-02 18:44:58 +0300

    Create structure of application and added some classes for php

---
+++ backend/classes/Base.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace classes;
+
+// CREATE TABLE base (
+//     base_name VARCHAR(50) NOT NULL PRIMARY KEY,
+//     coordinates VARCHAR(50) NOT NULL,
+//     region_name VARCHAR(50) NOT NULL,
+//     waterbody_id INT NOT NULL,
+//     FOREIGN KEY (waterbody_id) REFERENCES waterbody (waterbody_id)
+// );
+
+class Base {
+    
+}
---
+++ backend/index.php
@@ -0,0 +1,5 @@
+<?php
+
+use classes\Base;
+use classes\WaterBody;
+use classes\Tariff;
